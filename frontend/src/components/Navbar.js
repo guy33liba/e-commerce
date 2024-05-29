09e14300c1fc20953340c11dbc16ae323afca769
Guy Liba <guy33liba@gmail.com>
2024-05-29 13:48:01 +0200

    fdasf

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,33 +3,42 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar">
-        <h2 style={{ marginRight: "300px" }}>
-          <Link style={{ color: "blue" }} to="/home">
-            My Shop
-          </Link>
-        </h2>
+      <div>
+        <div className="navbar">
+          <h2 style={{ marginLeft: "100px", marginRight: "100px" }}>
+            <Link style={{ color: "blue" }} to="/home">
+              My Shop
+            </Link>
+          </h2>
 
-        <h3>
-          <Link style={{ color: "blue" }} to="/home">
-            Home
-          </Link>
-        </h3>
-        <h3>
-          <Link style={{ color: "blue" }} to="/features">
-            Features
-          </Link>
-        </h3>
-        <h3>
-          <Link style={{ color: "blue" }} to="/pricing">
-            Pricing
-          </Link>
-        </h3>
-        <h3>
-          <Link style={{ color: "blue" }} to="/disabled">
-            Disabled
-          </Link>
-        </h3>
+          <h3>
+            <Link style={{ color: "blue" }} to="/features">
+              Features
+            </Link>
+          </h3>
+          <h3>
+            <Link style={{ color: "blue" }} to="/pricing">
+              Pricing
+            </Link>
+          </h3>
+          <h3>
+            <Link style={{ color: "blue" }} to="/disabled">
+              Disabled
+            </Link>
+          </h3>
+        </div>
+        <div>
+          <h3>
+            <Link style={{ color: "blue" }} to="/login">
+              Login
+            </Link>
+          </h3>
+          <h3>
+            <Link style={{ color: "blue" }} to="/cart">
+              Cart
+            </Link>
+          </h3>
+        </div>
       </div>
     </div>
   )
