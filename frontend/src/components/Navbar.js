c1b60507e6ef6f98573c54fd9b8ce768c836bb86
Guy Liba <guy33liba@gmail.com>
2024-05-29 13:36:57 +0200

    fas

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,19 +2,21 @@ import React from "react"
 import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
-    <div style={{ display: "flex", gap: "10px", marginLeft: "100px" }}>
-      <h3>
-        <Link to="/home">Home</Link>
-      </h3>
-      <h3>
-        <Link to="/features">Features</Link>
-      </h3>
-      <h3>
-        <Link to="/pricing">Pricing</Link>
-      </h3>
-      <h3>
-        <Link to="/disabled">Disabled</Link>
-      </h3>
+    <div>
+      <div className="navbar">
+        <h3>
+          <Link to="/home">Home</Link>
+        </h3>
+        <h3>
+          <Link to="/features">Features</Link>
+        </h3>
+        <h3>
+          <Link to="/pricing">Pricing</Link>
+        </h3>
+        <h3>
+          <Link to="/disabled">Disabled</Link>
+        </h3>
+      </div>
     </div>
   )
 }
