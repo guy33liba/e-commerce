8901f88496deb79f355eda7e816e91b57264d5c0
Guy Liba <guy33liba@gmail.com>
2024-05-29 15:55:35 +0200

    fdasfas

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,42 @@
-import { BsFillStarFill } from "react-icons/bs"
-import { BsStarHalf } from "react-icons/bs"
 import React from "react"
+import { BsFillStarFill, BsStarHalf, BsStar } from "react-icons/bs"
 import products from "../Products"
+
 const HomeScreen = () => {
+  const renderStars = (rating) => {
+    const fullStars = Math.floor(rating)
+    const halfStar = rating % 1 !== 0
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0)
+
+    return (
+      <>
+        {[...Array(fullStars)].map((_, index) => (
+          <BsFillStarFill key={`full-${index}`} />
+        ))}
+        {halfStar && <BsStarHalf />}
+        {[...Array(emptyStars)].map((_, index) => (
+          <BsStar key={`empty-${index}`} />
+        ))}
+      </>
+    )
+  }
+
   return (
     <div className="products">
-      {products.map((product) => {
-        return (
+      {products.map((product) => (
+        <div key={product.id}>
+          <img style={{ width: "200px", height: "200px" }} src={product.image} alt={product.name} />
           <div>
-            <img style={{ width: "200px", height: "200px" }} src={product.image} />
-            <div>
-              <BsStarHalf />
-              <BsFillStarFill />
-              {product.rating}
-            </div>
-            <div  className="name">{product.name}</div>
-            <div>{product.reviews}</div>
-            <div>{product.price}</div>
-
-            <div>{product.category}</div>
-            <div>{product.countInStock}</div>
+            {renderStars(product.rating)}
+            {product.rating}
           </div>
-        )
-      })} 
+          <div className="name">{product.name}</div>
+          <div>{product.reviews}</div>
+          <div>{product.price}</div>
+          <div>{product.category}</div>
+          <div>{product.countInStock}</div>
+        </div>
+      ))}
     </div>
   )
 }
