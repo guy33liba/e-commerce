33f2114b6c85e094c17b2429b0e19f0e48bb3eab
Guy Liba <guy33liba@gmail.com>
2024-06-23 13:01:23 +0200

    as

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,21 @@ import cors from "cors"
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.get("/", (req, res) => {
-  res.send("hello g")
+
+app.get("/api/products", async (req, res) => {
+  try {
+    // Fetch all products from the database
+    const allProducts = await Product.find()
+
+    // Send the entire products array as JSON response
+    res.json(allProducts)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
 app.listen(4000, console.log("online"))
